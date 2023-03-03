494b761f0b95cb79af08eca0ac8a70cc3860c0c8
Razi Ullah <raziullah913@gmail.com>
2023-03-03 19:58:05 +0500

    inheritance further explained via app

---
+++ Multi-Level Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace IDK
+{
+    public class GrandParent
+    {
+        public void Printer()
+        {
+            Console.WriteLine("Grandparent Class Method!");
+        }
+    }
+
+    public class Parent : GrandParent
+    {
+        public void Printer()
+        {
+            Console.WriteLine("Parent Class Method!");
+        }
+    }
+
+    public class Child : Parent { }
+
+
+    class Program
+    {
+        public static void Main()
+        {
+            GrandParent child1 = new Child();
+            child1.Printer();
+
+            Child child2 = new Child();
+            ((GrandParent)(child2)).Printer();
+        }
+    }
+}
