490df6cca7fd148f3b343c8b4a2095a9b329bd80
Razi Ullah <raziullah913@gmail.com>
2023-03-03 15:15:26 +0500

    minor bug fixed

--- Area & Perimeter/Shape.cs
+++ Area & Perimeter/Shape.cs
@@ -2,13 +2,13 @@
 
 // Shape is the parent class of Triangle(form2)
 // and Rectangle(form3) classes
-public class Shape
+public abstract class Shape
 {
     // most shapes will need these to be computed
     protected double area;
     protected double perimeter;
     // writing virtual methods to make their
     // implementation in derived classes compulsory
-    public virtual void Area() { area = 0; }
-    public virtual void Perimeter() { perimeter = 0; }
+    public abstract void Area();
+    public abstract void Perimeter();
 }
