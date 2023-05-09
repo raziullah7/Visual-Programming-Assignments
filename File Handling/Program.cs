cdb68dada2aae95fc6906fb893abc8f0a4269b2c
Razi Ullah <raziullah913@gmail.com>
2023-05-09 17:48:59 +0500

    updating File Handling projects with other basics of file handling in C#

--- File Handling/Program.cs
+++ File Handling/Program.cs
@@ -1,27 +1,61 @@
 ﻿
 
-// writing using StreamWriter class
+//// writing using StreamWriter class
+//try
+//{
+//    StreamWriter writer = new StreamWriter("C:\\Users\\DELL\\Downloads\\OneDrive\\Desktop\\FileHandling.txt");
+//    writer.Write("\nIT WORKSSS!!");
+//    writer.Write("\nOR DOES IT?");
+//    writer.Close();
+//}
+//catch (Exception e)
+//{
+//    Console.WriteLine("Exception" + e.Message);
+//    // Console.WriteLine(e.ToString());
+//}
+
+
+//// reading using StreamReader class
+//try
+//{
+//    StreamReader reader = new StreamReader("C:\\Users\\DELL\\Downloads\\OneDrive\\Desktop\\FileHandling.txt");
+
+//    Console.WriteLine(reader.ReadToEnd());
+
+//    //string line = "";
+//    //while (line != null)
+//    //{
+//    //    line = reader.ReadLine();
+//    //    Console.WriteLine(line);
+//    //}
+
+//    reader.Close();
+//}
+//catch(Exception e)
+//{
+//    Console.WriteLine("Exception" + e.Message);
+//    // Console.WriteLine(e.ToString());
+//}
+
+
+// using FileStream
+string path = ("C:\\Users\\DELL\\Downloads\\OneDrive\\Desktop\\Files\\FileHandling2.txt");
 try
 {
-    StreamWriter writer = new StreamWriter("C:\\Users\\DELL\\Downloads\\OneDrive\\Desktop\\FileHandling.txt");
-    writer.Write("\nIT WORKSSS!!");
-    writer.Write("\nOR DOES IT?");
-    writer.Close();
-}
-catch (Exception e)
-{
-    Console.WriteLine("Exception" + e.Message);
-    // Console.WriteLine(e.ToString());
-}
+    // for previously existing file
+    FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.ReadWrite);
 
+    // passing filestream in streamwriter
+    StreamWriter writer = new StreamWriter(fileStream);
+    // writer.WriteLine("This is the resistance");
+    // writer.WriteLine("You can't resist us");
 
-// reading using StreamReader class
-try
-{
-    StreamReader reader = new StreamReader("C:\\Users\\DELL\\Downloads\\OneDrive\\Desktop\\FileHandling.txt");
-
+    // passing streamreader in filestream
+    StreamReader reader = new StreamReader(fileStream);
+    // printing to console
     Console.WriteLine(reader.ReadToEnd());
 
+    //// iterating manually
     //string line = "";
     //while (line != null)
     //{
@@ -29,10 +63,12 @@ try
     //    Console.WriteLine(line);
     //}
 
+    // closing streams
+    writer.Close();
     reader.Close();
-}
-catch(Exception e)
+    fileStream.Close();
+
+}catch(Exception e)
 {
     Console.WriteLine("Exception" + e.Message);
-    // Console.WriteLine(e.ToString());
 }
